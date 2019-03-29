c80a02438797b37f0e596b76ec729154c1b9b475
adyarcafe <adyarcafe@gmail.com>
2019-03-29 09:29:17 +0530

    Signed-off-by: adyarcafe <adyarcafe@gmail.com>

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from pprint import pprint as pp
+from flask import Flask, flash, redirect, render_template, request, url_for
+from weather import query_api
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template(
+        'weather.html',
+        data=[{'name':'Chennai'}, {'name':'Virudhunagar'}, {'name':'Kollam'},
+        {'name':'Vellore'}, {'name':'Ooty'}, {'name':'Mysore'},
+        {'name':'Kodaikanal'}, {'name':'Chikmagalur'}])
+@app.route("/result" , methods=['GET', 'POST'])
+def result():
+    data = []
+    error = None
+    select = request.form.get('comp_select')
+    resp = query_api(select)
+    pp(resp)
+    if resp:
+       data.append(resp)
+    if len(data) != 2:
+        error = 'Bad Response from Weather API'
+    return render_template(
+        'result.html',
+        data=data,
+        error=error)
+if __name__=='__main__':
+    app.run(debug=True)
